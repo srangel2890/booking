aa809b1f953d17199d43dd333b537b3e1f49ede8
srangel2890 <sara.rangel.rico@gmail.com>
2026-09-01 16:38:02 -0600

    main #comment Limit API routes to implemented actions #time

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 
   Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
-        resources :events
-        resources :reservations
-        get "me", to: "auth#me"
-        post "signup", to: "auth#signup"
-        post "login", to: "auth#login"
+        resources :events, only: [:index, :show, :create]
+        resources :reservations, only: [:show, :create]
+
+        post "/login", to: "auth#login"
       end
     end
   end
